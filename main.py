014bc37d3486b583b5270061d22a934150ae0200
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-07-02 19:01:12 +1000

    make test xor more explicit

--- main.py
+++ main.py
@@ -8,16 +8,27 @@ def main():
 
 def xor(use_hf=True):
     """Run a basic xor training test.
-
     :param bool use_hf: if True run example using Hessian-free optimization,
         otherwise use stochastic gradient descent
     """
-
+    # data
     inputs = np.asarray([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
     targets = np.asarray([[0], [1], [1], [0]], dtype=np.float32)
 
-    ff = hf.FFNet([2, 5, 1])
+    # net
+    shape = [2, 5, 1]
+    ff = hf.FFNet(shape,
+                  layers=hf.nl.Logistic(),
+                  conns = None, # default is to connect in series
+                  loss_type=hf.loss_funcs.SquaredError(),
+                  W_init_params=None,
+                  use_GPU=False,
+                  load_weights=None,
+                  debug=False,
+                  rng=np.random.RandomState(12345),
+                  dtype=np.float32)
 
+    # train
     if use_hf:
         ff.run_epochs(inputs, targets,
                       optimizer=hf.opt.HessianFree(CG_iter=2),
@@ -27,6 +38,8 @@ def xor(use_hf=True):
         ff.run_epochs(inputs, targets, optimizer=hf.opt.SGD(l_rate=1),
                       max_epochs=100, plotting=True)
 
+    # test
+    print('### testing...')
     outputs = ff.forward(inputs)[-1]
     for i in range(4):
         print("-" * 2)
